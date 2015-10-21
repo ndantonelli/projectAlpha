da422f5f97a0ed88706c9345ade813e32c540a3b
Nicholas Antonelli <ndantonelli@Nicholass-MacBook-Air.local>
2015-10-20 22:07:30 -0500

    added register php

--- api/index.php
+++ api/index.php
@@ -2,14 +2,30 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-//$database = new mysqli("localhost", "root", "12345678", "BurritoDB" );
+global $db = new mysqli("localhost", "root", "Foundry", "FoundryDB" );
 
 $app->post('/auth', function(){
 	echo -1;
 });
 
 $app->post('/user', function(){
-	echo -1;
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];
+	$first = $_POST['first'];
+	$last = $_POST['last'];
+	$area = $_POST['area'];
+	$num = $_POST['num'];
+	$tut = $_POST['tut'];
+
+	$result = $db->query("SELECT id FROM users WHERE email = '$email' OR (num = '$num' AND area = '$area'");
+	if($result->num_rows > 0)
+		echo json_encode(id => -1);
+	else{
+		$db->query("INSERT INTO user(email, pass, first, last, area, num, tutor)
+					VALUES ('$email', '$pass', '$first', '$last', '$area', '$num', '$tut') ");
+		$result = $db->query("SELECT LAST_INSERT_ID()");
+		echo $result->fetch_assoc();
+	}
 });
 
 $app->run();
