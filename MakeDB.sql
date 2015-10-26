36bd5ddfbdb02a45c2f67b24c318a61efe433b83
Nicholas Antonelli <ndantonelli@Nicholass-MacBook-Air.local>
2015-10-26 01:05:36 -0500

    more complete database creation script

--- MakeDB.sql
+++ MakeDB.sql
@@ -14,7 +14,9 @@ url VARCHAR(100) DEFAULT 'https://acrobatusers.com/assets/images/template/author
 area VARCHAR(5) NOT NULL,
 num VARCHAR(10) NOT NULL,
 salt VARCHAR(50) NOT NULL,
-tutor INT NOT NULL
+location VARCHAR(100) DEFAULT "",
+rating FLOAT(2,1) DEFAULT 0.0,
+num_ratings INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS topics(
@@ -29,13 +31,49 @@ name VARCHAR(100) NOT NULL,
 url VARCHAR(100) NOT NULL,
 FOREIGN KEY (tid) REFERENCES topics(id));
 
+-- Max price per hour is $90.90
 CREATE TABLE IF NOT EXISTS tutors(
 uid INT NOT NULL,
 tid INT NOT NULL,
 sid INT NOT NULL,
 status ENUM("active", "busy", "offline") DEFAULT "offline",
+price FLOAT(4,2) DEFAULT 00.00,
+travel ENUM("driving", "walking", "bicycling") DEFAULT "driving",
 PRIMARY KEY (uid,tid,sid),
 FOREIGN KEY (uid) REFERENCES users(id),
 FOREIGN KEY (tid) REFERENCES topics(id),
 FOREIGN KEY (sid) REFERENCES subs(id)
+);
+
+-- Max hours for one session is 11
+-- pay per quarter of an hour (round up)
+CREATE TABLE IF NOT EXISTS session(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+sid INT NOT NULL,
+tid INT NOT NULL,
+tut_id INT NOT NULL,
+stud_id INT NOT NULL,
+start TIMESTAMP,
+end TIMESTAMP,
+tot_time float (4,2) DEFAULT 00.00,
+price FLOAT(4,2) DEFAULT 00.00,
+tot_price FLOAT(5,2) DEFAULT 000.00,
+FOREIGN KEY (tut_id) REFERENCES users(id),
+FOREIGN KEY (stud_id) REFERENCES users(id),
+FOREIGN KEY (tid) REFERENCES topics(id),
+FOREIGN KEY (sid) REFERENCES subs(id),
+FOREIGN KEY (price) REFERENCES tutors(price)
+);
+
+CREATE TABLE IF NOT EXISTS ratings(
+	uid INT NOT NULL,
+	sesh_id INT NOT NULL,
+	stud_id INT NOt NULL,
+	rate INT NOT NULL,
+	comment VARCHAR(150) DEFAULT "",
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+	PRIMARY KEY (uid, sesh_id),
+	FOREIGN KEY (uid) REFERENCES users(id),
+	FOREIGN KEY (stud_id) REFERENCES users(id),
+	FOREIGN KEY (sesh_id) REFERENCES session(id)
 );
